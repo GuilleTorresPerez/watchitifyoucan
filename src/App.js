954d1336898c7925b2b2b8736355c5c4e71b94eb
Guillermo Torres Pérez <60155590+GuilleTorresPerez@users.noreply.github.com>
2024-12-20 20:29:01 +0200

    Fix tailwind implementation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import React from 'react';
 import Saludo from './Saludo';
+
+function Home() {
+  return <h1>Página de Inicio</h1>;
+}
+
+function Movies() {
+  return <h1>This is a list of movies</h1>;
+}
 
 function App() {
   return (
-    <div>
-      <Saludo nombre="Juan"/>
-      <Saludo nombre="Hola"/>
+    <Router>
+      <nav>
+          <Link to="/">WatchMeIfYouCan</Link>
+          <Link to="/movies">Movies</Link>
+      </nav>
 
-    </div>
+
+
+
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+      </Routes>
+    </Router>
   );
 }
 
