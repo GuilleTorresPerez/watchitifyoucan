93117f8ff0758c03bec374e632fee51324c9a6c4
Guillermo Torres Pérez <guilletorresperez@gmail.com>
2024-12-24 15:29:00 +0100

    Add tmdb service

--- src/components/popularMovies.js
+++ src/components/popularMovies.js
@@ -1,4 +1,14 @@
 import { useState } from 'react';
+import tmdbService from '../services/tmdbService';
+
+const loadPopularMovies = async () => {
+    try {
+      const movies = await tmdbService.getPopularMovies(1);
+      console.log('Popular Movies:', movies.results);
+    } catch (error) {
+      console.error('Error loading popular movies:', error);
+    }
+  };
 
 function popularMovies(props) {
     // const [contador, setContador] = useState(0);
@@ -7,7 +17,9 @@ function popularMovies(props) {
     return (
         <div>
             <h1>Popular movies</h1>
-
+            <button onClick={loadPopularMovies}>
+                Load popular movies
+            </button>
 
         </div>
     );
---
+++ src/services/tmdbService.js
@@ -0,0 +1,36 @@
+const API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMGFkYjg1MzQyZTViNGU1YmM1NTk5MWRlNDM5OThkNSIsIm5iZiI6MTczNDY1NTMxNS4zMiwic3ViIjoiNjc2NGJkNTM4Y2E1M2NjNmE3NWRlMjMwIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.Zqci6_t-x8ona4xkk-mZ-T0OPXWtbfk2TnqikSsNVlQ';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const AUTH_TOKEN = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMGFkYjg1MzQyZTViNGU1YmM1NTk5MWRlNDM5OThkNSIsIm5iZiI6MTczNDY1NTMxNS4zMiwic3ViIjoiNjc2NGJkNTM4Y2E1M2NjNmE3NWRlMjMwIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.Zqci6_t-x8ona4xkk-mZ-T0OPXWtbfk2TnqikSsNVlQ';  
+
+const makeRequest = async (endpoint, params = {}) => {
+    const url = new URL(`${BASE_URL}${endpoint}`);
+    Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
+
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: AUTH_TOKEN,
+        },
+    }
+
+    try {
+        const response = await fetch(url, options);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return await response.json();
+    } catch (error) {
+        console.error('There was a problem with your fetch operation:', error);
+        throw error;
+    }
+};
+
+export const getPopularMovies = async (page = 1) => {
+    return makeRequest('/movie/popular', { page, api_key: API_KEY });
+
+};
+
+export default {
+    getPopularMovies,
+};
