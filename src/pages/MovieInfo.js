dc1cc6af02d302d0d8f29ff3275a77ea975b61c4
Guillermo Torres Pérez <60155590+GuilleTorresPerez@users.noreply.github.com>
2024-12-28 15:42:20 +0200

    Add styling to movie info page

--- src/pages/MovieInfo.js
+++ src/pages/MovieInfo.js
@@ -11,12 +11,14 @@ const MovieInfo = () => {
       try {
         const movieData = await tmdbService.getMovieDetails(id); // Llama al servicio para obtener los detalles
         setMovie(movieData);
+        console.log('Movie:', movieData);
       } catch (error) {
         console.error('Error fetching movie details:', error);
       }
     };
 
     fetchMovie();
+    
   }, [id]);
 
   if (!movie) {
@@ -24,19 +26,25 @@ const MovieInfo = () => {
   }
 
   return (
-    <div>
-      <h1>{movie.title}</h1>
-      <img
-        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-        alt={movie.title}
-      />
-      <p>{movie.overview}</p>
-      <p>Release Date: {movie.release_date}</p>
-      <p>Rating: {movie.vote_average}/10</p>
-      <p>Genres: {movie.genres.map((genre) => genre.name).join(', ')}</p>
+    <div className='flex flex-col items-center min-h-screen bg-gray-900 text-white px-28 py-6'>
+      <div className='flex flex-col md:flex-row gap-10 w-full'>
+        <div>
+          <img
+            className='w-[500px] rounded shadow-lg'
+            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+            alt={movie.title}
+          />
+        </div>
+        <div className='p-5 space-y-3'>
+          <h1 className='text-3xl font-bold'>{movie.title}</h1>
+          <p>{movie.overview}</p>
+          <p>Release Date: {movie.release_date}</p>
+          <p>Rating: {movie.vote_average}/10</p>
+          <p>Genres: {movie.genres.map((genre) => genre.name).join(', ')}</p>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default MovieInfo;
-    
